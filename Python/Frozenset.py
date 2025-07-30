9fc032646fc9fa9725a5e8baf02a7310b2bdb44d
HarishKalamanda79 <kalamandaharish79@gmail.com>
2025-07-30 23:26:33 +0530

    Frozenset.

---
+++ Python/Frozenset.py
@@ -0,0 +1,9 @@
+#Frozenset
+f = frozenset([8, 2, 6]) #immutable
+print(type(f))
+
+# f = frozenset(5) is not applicable
+
+l = [5, 8, 15, 25]
+a = frozenset(l) # converting to immutable => unchangalbe 
+print(a)
